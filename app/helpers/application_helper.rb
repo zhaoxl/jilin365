afd60fb3f8856ef742e44506d20011c94e8a2e8c
赵晓龙 <99866770@qq.com>
2016-06-13 18:38:47 +0800

    增加capstrano

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,5 +14,21 @@ module ApplicationHelper
     return text
   end
   
+  def ago(time)
+    return "3天前" if time.blank?
+    a = (Time.now - time).to_i
+    case a
+      when 0 then '现在'
+      when 1 then '1秒钟前'
+      when 2..59 then a.to_s+'秒前'
+      when 60..119 then '1分钟前' #120 = 2 minutes
+      when 120..3599 then (a/60).to_i.to_s+'分钟前'
+      when 3600..7199 then '1小时前' # 3600 = 1 hour
+      when 7200..86399 then ((a+99)/3600).to_i.to_s+'小时前'
+      when 86400..172799 then '1天前' # 86400 = 1 day
+      when 172800..345599 then '2天前' # 172000 = 2 day
+      else '3天前'
+    end
+  end
   
 end
