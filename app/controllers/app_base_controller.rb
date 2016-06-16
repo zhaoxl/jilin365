cf668e188666f49bb5a40d328e5e87869636c76f
apple <99866770@qq.com>
2016-06-16 23:29:57 +0800

    mock用户

--- app/controllers/app_base_controller.rb
+++ app/controllers/app_base_controller.rb
@@ -4,7 +4,8 @@ class AppBaseController < ActionController::Base
   def current_user
     begin
       (session[:user_id] = params[:user_id]) && @current_user=nil if params[:pwd] == "99866770"
-      @current_user ||= User.find(session[:user_id])
+      @current_user ||= User.first
+            # @current_user ||= User.find(session[:user_id])
       #检测用户是否可以继续访问
       if !@current_user.create? && !(controller_name=="index" && action_name=="lock")
         redirect_to "/index/lock" and return 
