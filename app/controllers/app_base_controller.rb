c209353afadb2d54ea1512c386b5a91ed0ab030f
赵晓龙 <99866770@qq.com>
2016-06-17 14:21:46 +0800

    调试微信登录

--- app/controllers/app_base_controller.rb
+++ app/controllers/app_base_controller.rb
@@ -1,5 +1,9 @@
 class AppBaseController < ActionController::Base
   layout "application"
+  class UserNotLoginException < Exception
+  end
+  class UserLockException < Exception
+  end
   
   def current_user
     begin
@@ -7,14 +11,34 @@ class AppBaseController < ActionController::Base
       @current_user ||= User.find(session[:user_id])
       #检测用户是否可以继续访问
       if !@current_user.create? && !(controller_name=="index" && action_name=="lock")
-        redirect_to "/index/lock" and return 
+        raise UserLockException
+        # redirect_to "/index/lock" and return
       else
         return @current_user
       end
     rescue
-      redirect_to "/wechat/login" and return
+      raise UserNotLoginException
+      # redirect_to "/wechat/login" and return
+    end
+  end
+
+  rescue_from Exception, with: :rescue_action
+
+  def rescue_action(ex)
+    case ex
+    when UserNotLoginException then
+      redirect_to "/wechat/login?goto=#{request.url}" and return
+    when UserLockException then
+      redirect_to "/index/lock" and return
+    else
+      raise ex
     end
   end
   
   
 end
+
+
+
+
+
