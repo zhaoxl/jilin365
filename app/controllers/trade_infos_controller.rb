9f461db85bb85c68736ad63dacd04cc34e67612c
赵晓龙 <99866770@qq.com>
2016-06-20 19:24:06 +0800

    完善搜索功能 修改浮动页脚

--- app/controllers/trade_infos_controller.rb
+++ app/controllers/trade_infos_controller.rb
@@ -3,11 +3,12 @@ class TradeInfosController < AppBaseController
     @root_category = TradeInfoCategory.find(params[:rc]) rescue nil
     @sub_categories = @root_category.children rescue []
     @areas = cookies[:city_code].present? ? Area.where(parent_code: cookies[:city_code]) : []
-    
-    @trade_infos = TradeInfo.where(trade_info_category: @sub_categories) if params[:c].blank?
-    @trade_infos = TradeInfo.where(trade_info_category_id: params[:c]) if params[:c].present?
+
+    @trade_infos = TradeInfo.where(city_code: cookies[:city_code]) if cookies[:city_code].present?
+    @trade_infos = @trade_infos.where(trade_info_category: @sub_categories) if params[:c].blank? && @sub_categories.present?
+    @trade_infos = @trade_infos.where(trade_info_category_id: params[:c]) if params[:c].present?
     @trade_infos = @trade_infos.where(district_code: params[:a]) if params[:a].present?
-    @trade_infos = @trade_infos.where("title LIKE ?", "%#{k}%") if params[:k].present?
+    @trade_infos = @trade_infos.where("title LIKE ?", "%#{params[:k]}%") if params[:k].present?
     @trade_infos = @trade_infos.where(state: :payment)
   end
   
@@ -19,7 +20,7 @@ class TradeInfosController < AppBaseController
       redirect_to index_index_path and return 
     end
     @collected = current_user.collects.where(item: @trade_info).exists?
-    @similarity_trade_infos = TradeInfo.where(state: :payment).order("rand()").limit(3)
+    @similarity_trade_infos = TradeInfo.where(state: :payment).where(city_code: cookies[:city_code]).order("rand()").limit(3)
   end
   
   def like
